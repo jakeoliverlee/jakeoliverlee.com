4d44b293de6c0606ed67132956369a10e42a51f0
Jake <jakeoliverlee@gmail.com>
2023-04-01 11:32:30 +0100

    testing site visit counter with test ejs template

--- src/expressjs/index.js
+++ src/expressjs/index.js
@@ -9,6 +9,14 @@ var indexRouter = require("../expressjs/routes/routes");
 
 
 const app = express();
+const url = "mongodb://localhost:27017/visitors";
+const con = mongoose.connect(url);
+
+connect.then((db) =>{
+    console.log("Successfully connected to visitorsDB!");
+})
+.catch((err)=>{console.log(err)});
+
 
 app.use(express.static(__dirname + "/pages/home"));
 app.use(express.static(__dirname + "/pages/contact"));
@@ -20,6 +28,10 @@ app.get("/contact", function(request, response){
 app.get("/", function(request, response){
     response.sendFile(__dirname + "/pages/home/index.html");
 });
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.listen(3000, function(){
     console.log("Server started on :3000")
