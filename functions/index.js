c07cd2966df2c41e467a735b2af1b4ecaac256e8
Jake <jakeoliverlee@gmail.com>
2023-05-14 10:42:07 +0100

    fix file path for gcp functions

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/download', (req, res) => {
-      const filePath = path.join(__dirname, '../src/pages/home/assets/resume/Jake Lee Resume.docx');
-      res.download(filePath, 'Jake Lee Resume.docx',);
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'resume', 'Jake Lee Resume.docx');
+  res.download(filePath, 'Jake Lee Resume.docx');
 });
 
 exports.app = functions.https.onRequest(app);
