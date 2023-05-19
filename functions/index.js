83aeb6332ae7090d2bae3acbd89f0e0f17134ccc
Jake <jakeoliverlee@gmail.com>
2023-05-19 14:11:36 +0100

    add cloud function for tracking new visitors

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,22 @@ const functions = require('firebase-functions');
 const express = require('express');
 const path = require('path');
 const app = express();
+const admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
+admin.initializeApp();
+
+exports.countVisit = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    const doc = admin.firestore().doc('pages/main');
+    const page = await doc.get();
+    const pageData = page.data();
+    const currentCount = pageData ? pageData.count : 0;
+    const newCount = (currentCount || 0) + 1;
+    await doc.set({ count: newCount });
+    res.json({ count: newCount });
+  });
+});
+
 
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, 'resume', 'Jake Lee Resume.docx');
